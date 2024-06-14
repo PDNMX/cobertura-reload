13c81b20485d49d1b739e3cd5129a0aa2dbcd319
RBalan21 <aln.rojas.pdn@gmail.com>
2024-06-14 13:03:53 -0600

    iconos sistema

--- components/tables/entes-table/columns.tsx
+++ components/tables/entes-table/columns.tsx
@@ -2,7 +2,7 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { CellAction } from "./cell-action";
 import { User } from "@/constants/data";
-import { Checkbox } from "@/components/ui/checkbox";
+import { CheckCircle, XCircle } from "lucide-react";
 
 export const columns: ColumnDef<User>[] = [
   {
@@ -13,7 +13,6 @@ export const columns: ColumnDef<User>[] = [
     accessorKey: "ambitoGobierno",
     header: "Ambito Gobierno",
   },
-
   {
     accessorKey: "poderGobierno",
     header: "Poder Gobierno",
@@ -21,18 +20,54 @@ export const columns: ColumnDef<User>[] = [
   {
     accessorKey: "sistema1",
     header: "S1",
+    cell: ({ row }) => (
+      <div className="flex justify-center items-center h-full">
+        {row.original.sistema1 ? (
+          <CheckCircle className="text-green-500" />
+        ) : (
+          <XCircle className="text-red-500" />
+        )}
+      </div>
+    ),
   },
   {
     accessorKey: "sistema2",
     header: "S2",
+    cell: ({ row }) => (
+      <div className="flex justify-center items-center h-full">
+        {row.original.sistema2 ? (
+          <CheckCircle className="text-green-500" />
+        ) : (
+          <XCircle className="text-red-500" />
+        )}
+      </div>
+    ),
   },
   {
     accessorKey: "sistema3",
     header: "S3",
+    cell: ({ row }) => (
+      <div className="flex justify-center items-center h-full">
+        {row.original.sistema3 ? (
+          <CheckCircle className="text-green-500" />
+        ) : (
+          <XCircle className="text-red-500" />
+        )}
+      </div>
+    ),
   },
   {
     accessorKey: "sistema6",
     header: "S6",
+    cell: ({ row }) => (
+      <div className="flex justify-center items-center h-full">
+        {row.original.sistema6 ? (
+          <CheckCircle className="text-green-500" />
+        ) : (
+          <XCircle className="text-red-500" />
+        )}
+      </div>
+    ),
   },
   {
     id: "actions",
--- constants/data.ts
+++ constants/data.ts
@@ -1,6 +1,10 @@
 import { NavItem } from "@/types";
 
 export type User = {
+  sistema1: any;
+  sistema2: any;
+  sistema3: any;
+  sistema6: any;
   id: number;
   name: string;
   company: string;
