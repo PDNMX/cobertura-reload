71026cb287feb49350d496640f47ffd14f97cc9e
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2024-06-21 14:48:49 -0600

    tamaño texto sin resultados

--- components/layout/ThemeToggle/theme-toggle.tsx
+++ components/layout/ThemeToggle/theme-toggle.tsx
@@ -3,8 +3,7 @@ import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-type CompProps = {};
-export default function ThemeToggle({}: CompProps) {
+export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   return (
     <>
@@ -17,7 +16,7 @@ export default function ThemeToggle({}: CompProps) {
         }}>
         <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <span className="sr-only">Toggle theme</span>
+        <span className="sr-only">Cambiar tema de colores</span>
       </Button>
     </>
   );
