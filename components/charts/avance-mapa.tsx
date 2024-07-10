3e870aa5d4a8814236f2a2e2fcb9aa943b3481d4
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2024-07-10 16:10:26 -0600

    mejorando mapa

--- components/charts/avance-mapa.tsx
+++ components/charts/avance-mapa.tsx
@@ -3,15 +3,19 @@ import React, { useState, useMemo } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import { Tooltip } from "react-tooltip";
 import dataMex from "./data-mexico";
-import { scaleLinear } from "d3-scale";
+import { scalePow } from "d3-scale";
 import { interpolateRgb } from "d3-interpolate";
 
 const generateColorRange = (baseColor: string) => {
-  const lighterColor = interpolateRgb(baseColor, "#ffffff")(0.9);
-  const darkerColor = interpolateRgb(baseColor, "#000000")(0.1);
-  return scaleLinear<string>()
-    .domain([0, 100])
-    .range([lighterColor, darkerColor]);
+  const lighterColor = interpolateRgb(baseColor, "#ffffff")(0.99);
+  const darkerColor = interpolateRgb(baseColor, "#000000")(0.01);
+
+  // Usar una escala de potencia en lugar de una escala lineal
+  return scalePow<string>()
+    .exponent(0.3) // Ajusta este valor para controlar la curvatura de la escala
+    .domain([0, 50, 100])
+    .range([lighterColor, baseColor, darkerColor])
+    .clamp(true); // Asegura que los valores fuera del dominio se ajusten al rango
 };
 
 export const AvanceMapa = ({
@@ -22,8 +26,11 @@ export const AvanceMapa = ({
   baseColor?: string;
 }) => {
   const [tooltipContent, setTooltipContent] = useState("");
-  
-  const colorScale = useMemo(() => generateColorRange(baseColor, 10), [baseColor]);
+
+  const colorScale = useMemo(
+    () => generateColorRange(baseColor, 10),
+    [baseColor],
+  );
 
   return (
     <>
@@ -33,29 +40,42 @@ export const AvanceMapa = ({
         projectionConfig={{
           center: [-102, 24],
           scale: 1450,
-        }}
-      >
+        }}>
         <Geographies geography={dataMex}>
           {({ geographies }: any) =>
             geographies.map((geo: any) => {
               const porcentajeAvance = data.find(
-                (s: any) => s.entidad == geo.properties.clave
+                (s: any) => s.entidad == geo.properties.clave,
               );
               const percentage = porcentajeAvance ? porcentajeAvance.count : 0;
               return (
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
-                  fill={porcentajeAvance ? colorScale(percentage) : "#d7d7d7"}
-                  stroke="#535658"
+                  fill={porcentajeAvance ? colorScale(percentage) : "#2A272A"}
+                  stroke="#2A272A"
+                  strokeWidth={1.5}
                   style={{
+                    default: {
+                      outline: "none",
+                    },
                     hover: {
-                      stroke: "#d7d7d7",
+                      fill: porcentajeAvance
+                        ? interpolateRgb(colorScale(percentage), "#000000")(0.3)
+                        : interpolateRgb("#2A272A", "#000000")(0.3),
+                      stroke: "#2A272A",
+                      strokeWidth: 2.5,
+                      outline: "none",
+                      transition: "all 250ms",
                     },
                   }}
                   onMouseEnter={() => {
                     setTooltipContent(
-                      `${porcentajeAvance ? porcentajeAvance.nombreEntidad : 'N/A'}: ${percentage}%`
+                      `${
+                        porcentajeAvance
+                          ? porcentajeAvance.nombreEntidad
+                          : "N/A"
+                      }: ${percentage}%`,
                     );
                   }}
                   onMouseLeave={() => {
